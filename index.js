5e2eaa6f9381f0579fc47a31e94ac623776747e2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-24 23:15:53 +0200

    express template

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("test server")
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 5000
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
