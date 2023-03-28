66899934966e9ed76c256db09a4fe947d0d53e00
bogdan <bogdan.cutean@student.upt.ro>
2023-03-28 14:36:51 +0300

    DB test integration

---
+++ db.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql2');
+
+let connection = mysql.createConnection({
+    // In PROD this will be with ENV variables
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sys'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+
+  module.exports = connection;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
+let db = require('./db');
 const port = process.env.PORT || 5000
 
 
@@ -9,6 +10,14 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+//temp test on DB
+db.query(
+    'SELECT * FROM `users_table`',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+    }
+  );
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
