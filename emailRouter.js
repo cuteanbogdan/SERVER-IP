9efad9e484a113e7bb7bc3d85fac0ff61e635739
bogdan <bogdan.cutean@student.upt.ro>
2023-05-11 11:02:09 +0300

    updated email logic

--- emailRouter.js
+++ emailRouter.js
@@ -1,147 +1,203 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./db.js');
-const bcrypt = require('bcrypt');
-const { uuid } = require('uuidv4');
-const nodemailer = require('nodemailer');
+const db = require("./db.js");
+const bcrypt = require("bcrypt");
+const { uuid } = require("uuidv4");
+const nodemailer = require("nodemailer");
 
 //send email
 function sendEmail(email, token) {
+  var email = email;
+  var token = token;
 
-    var email = email;
-    var token = token;
+  var mail = nodemailer.createTransport({
+    service: "yahoo",
+    auth: {
+      user: "cutybogdy@yahoo.com", // Your email id
+      pass: "dbyvstclmtlnkord", // Your password
+    },
+  });
 
-    var mail = nodemailer.createTransport({
-        service: 'yahoo',
-        auth: {
-            user: 'cutybogdy@yahoo.com', // Your email id
-            pass: 'dbyvstclmtlnkord' // Your password
-        }
-    });
+  var mailOptions = {
+    from: "cutybogdy@yahoo.com",
+    to: email,
+    subject: "Reset Password Link - SmartCare.com",
+    html:
+      '<p>You requested for reset password, kindly use this <a href="http://localhost:3000/update-password?token=' +
+      token +
+      '">link</a> to reset your password</p>',
+  };
 
-    var mailOptions = {
-        from: 'cutybogdy@yahoo.com',
-        to: email,
-        subject: 'Reset Password Link - SmartCare.com',
-        html: '<p>You requested for reset password, kindly use this <a href="http://localhost:3000/update-password?token=' + token + '">link</a> to reset your password</p>'
-
-    };
-
-    mail.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(1)
-        } else {
-            console.log(0)
-        }
-    });
+  mail.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(1);
+    } else {
+      console.log(0);
+    }
+  });
 }
 /* send reset password link in email */
-router.post('/change-password-email', async function (req, res) {
-    try {
-        const email = req.body.email;
+router.post("/change-password-email", async function (req, res) {
+  try {
+    const email = req.body.email;
 
-        db.query('SELECT * FROM users_database WHERE email = ?', [email], async function (err, result) {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({
-                    status: 'error',
-                    msg: 'Internal server error'
-                });
-            }
+    db.query(
+      `(
+            SELECT email, 'Administratori' as table_name FROM Administratori WHERE LOWER(email) = LOWER(${db.escape(
+              email
+            )})
+        ) UNION (
+            SELECT email, 'Pacienti' as table_name FROM Pacienti WHERE LOWER(email) = LOWER(${db.escape(
+              email
+            )})
+        ) UNION (
+            SELECT email, 'Doctori' as table_name FROM Doctori WHERE LOWER(email) = LOWER(${db.escape(
+              email
+            )})
+        ) UNION (
+            SELECT email, 'Ingrijitori' as table_name FROM Ingrijitori WHERE LOWER(email) = LOWER(${db.escape(
+              email
+            )})
+        ) UNION (
+            SELECT email, 'Supraveghetori' as table_name FROM Supraveghetori WHERE LOWER(email) = LOWER(${db.escape(
+              email
+            )})
+        );`,
+      async function (err, result) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: "error",
+            msg: "Internal server error",
+          });
+        }
 
-            if (result.length > 0) {
-                var changePasswordToken = uuid();
-                var sent = await sendEmail(email, changePasswordToken);
+        if (result.length > 0) {
+          var changePasswordToken = uuid();
+          var sent = await sendEmail(email, changePasswordToken);
 
-                if (sent != '0') {
-                    var data = {
-                        changePasswordToken: changePasswordToken
-                    };
+          if (sent != "0") {
+            var data = {
+              changePasswordToken: changePasswordToken,
+            };
 
-                    db.query('UPDATE users_database SET ? WHERE email = ?', [data, email], function (err, result) {
-                        if (err) {
-                            console.error(err);
-                            return res.status(500).json({
-                                status: 'error',
-                                msg: 'Internal server error'
-                            });
-                        }
-                    });
-
-                    res.status(200).json({
-                        status: 'success',
-                        msg: 'The reset password link has been sent to your email address: '
-                    });
-                } else {
-                    res.status(500).json({
-                        status: 'error',
-                        msg: 'Something went wrong. Please try again'
-                    });
+            db.query(
+              `UPDATE ${result[0].table_name} SET ? WHERE email = ?`,
+              [data, email],
+              function (err, result) {
+                if (err) {
+                  console.error(err);
+                  return res.status(500).json({
+                    status: "error",
+                    msg: "Internal server error",
+                  });
                 }
-            } else {
-                res.status(404).json({
-                    status: 'error',
-                    msg: 'The Email is not registered with us'
-                });
-            }
-        });
-    } catch (error) {
-        console.error("ERROR", error.name);
-        res.status(500).json({ msg: 'Internal server error' });
-    }
+              }
+            );
+
+            res.status(200).json({
+              status: "success",
+              msg: "The reset password link has been sent to your email address: ",
+            });
+          } else {
+            res.status(500).json({
+              status: "error",
+              msg: "Something went wrong. Please try again",
+            });
+          }
+        } else {
+          res.status(404).json({
+            status: "error",
+            msg: "The Email is not registered with us",
+          });
+        }
+      }
+    );
+  } catch (error) {
+    console.error("ERROR", error.name);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 });
 
 /* update password to database */
-router.post('/update-password', function (req, res, next) {
-    try {
-        const token = req.body.token;
-        const password = req.body.password;
+router.post("/update-password", function (req, res, next) {
+  try {
+    const token = req.body.token;
+    const password = req.body.password;
 
-        db.query('SELECT * FROM users_database WHERE changePasswordToken ="' + token + '"', function (err, result) {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({
-                    status: 'error',
-                    msg: 'Internal server error'
-                });
-            }
+    db.query(
+      `(
+          SELECT email, 'Administratori' as table_name FROM Administratori WHERE changePasswordToken = ${db.escape(
+            token
+          )}
+        ) UNION (
+          SELECT email, 'Pacienti' as table_name FROM Pacienti WHERE changePasswordToken = ${db.escape(
+            token
+          )}
+        ) UNION (
+          SELECT email, 'Doctori' as table_name FROM Doctori WHERE changePasswordToken = ${db.escape(
+            token
+          )}
+        ) UNION (
+          SELECT email, 'Ingrijitori' as table_name FROM Ingrijitori WHERE changePasswordToken = ${db.escape(
+            token
+          )}
+        ) UNION (
+          SELECT email, 'Supraveghetori' as table_name FROM Supraveghetori WHERE changePasswordToken = ${db.escape(
+            token
+          )}
+        );`,
+      function (err, result) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: "error",
+            msg: "Internal server error",
+          });
+        }
 
-            if (result.length > 0) {
-                var saltRounds = 10;
-                bcrypt.genSalt(saltRounds, function (err, salt) {
-                    bcrypt.hash(password, salt, function (err, hash) {
-                        var data = {
-                            password: hash
-                        };
+        if (result.length > 0) {
+          var saltRounds = 10;
+          bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(password, salt, function (err, hash) {
+              var data = {
+                parola: hash,
+              };
 
-                        db.query('UPDATE users_database SET ? WHERE email ="' + result[0].email + '"', data, function (err, result) {
-                            if (err) {
-                                console.error(err);
-                                return res.status(500).json({
-                                    status: 'error',
-                                    msg: 'Internal server error'
-                                });
-                            }
-                        });
-
-                        res.status(200).json({
-                            status: 'success',
-                            msg: 'Your password has been updated successfully'
-                        });
+              db.query(
+                `UPDATE ${result[0].table_name} SET ? WHERE email = ${db.escape(
+                  result[0].email
+                )}`,
+                data,
+                function (err, result) {
+                  if (err) {
+                    console.error(err);
+                    return res.status(500).json({
+                      status: "error",
+                      msg: "Internal server error",
                     });
-                });
-            } else {
-                res.status(400).json({
-                    status: 'error',
-                    msg: 'Invalid link; please try again'
-                });
-            }
-        });
-    } catch (error) {
-        console.error("ERROR", error.name);
-        res.status(500).json({ msg: 'Internal server error' });
-    }
+                  }
+                }
+              );
 
+              res.status(200).json({
+                status: "success",
+                msg: "Your password has been updated successfully",
+              });
+            });
+          });
+        } else {
+          res.status(400).json({
+            status: "error",
+            msg: "Invalid link; please try again",
+          });
+        }
+      }
+    );
+  } catch (error) {
+    console.error("ERROR", error.name);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 });
 
 module.exports = router;
