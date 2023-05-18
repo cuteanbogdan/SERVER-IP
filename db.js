61a4518ac322d28545c8897bbb0adc646cb62bc8
BuildTools <unconfigured@null.spigotmc.org>
2023-05-18 22:58:32 +0300

    Db azure connection

--- db.js
+++ db.js
@@ -1,19 +1,22 @@
 var mysql = require('mysql2');
+var fs = require('fs');
 
 let connection = mysql.createConnection({
-    // In PROD this will be with ENV variables
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'aal'
+  // In PROD this will be with ENV variables
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: 3306,
+  ssl: { ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem") }
 });
 
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-  
-    console.log('Connected to the MySQL server.');
-  });
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
 
-  module.exports = connection;
+  console.log('Connected to the MySQL server on Azure.');
+});
+
+module.exports = connection;
